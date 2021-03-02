3ec3ac35f473a693f39f18a471489eee82fbc2a1
kuwamura <naoyan418@gmail.com>
2021-03-03 05:42:17 +0900

    Deploy website - based on bb0033e67f82d9e6ba4aa574b12eb106742fc939

---
+++ 04675bd5.802b70c1.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{72:function(o){o.exports=JSON.parse('{"allTagsPath":"/bingo-anke-site/blog/tags","slug":"facebook","name":"facebook","count":1,"permalink":"/bingo-anke-site/blog/tags/facebook"}')}}]);
---
+++ c8b7c27b.df5ec94f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[22],{93:function(a){a.exports=JSON.parse('{"allTagsPath":"/bingo-anke-site/blog/tags","slug":"hola","name":"hola","count":1,"permalink":"/bingo-anke-site/blog/tags/hola"}')}}]);
